acfff80814de4c0f7cf40796ee21884facab6614
Eric Petersen <eric@huskers.unl.edu>
2013-10-04 12:34:44 -0500

    Cleaning up remotefs a little bit

--- bash/remotefs.sh
+++ bash/remotefs.sh
@@ -3,39 +3,47 @@
 # (un)mount a remote filesystem over SSH at ~/remotefs
 #
 
+if ! TEST=$(which sshfs) ; then
+    echo "You must install sshfs."
+    exit 1
+fi
+
+REMOTE_HOST=$1
+ACTION=$2
+
 MOUNT_BASE=~/remotefs
+MOUNT_DIR="$MOUNT_BASE/$REMOTE_HOST"
 
 show_usage () {
     echo "usage: $0 <target> up|down"
-    exit 1
 }
 
-if [ -z "$1" ] ; then
+if [ -z "$REMOTE_HOST" ] ; then
     echo "No target specified!"
     show_usage 
+    exit 2
 fi
 
-case "$2" in
-    "up") ACTION=up ;;
-    "down") ACTION=down ;;
-    *) show_usage  ;;
-esac
-
-MOUNT_DIR="$MOUNT_BASE/$2"
+if [ -z "$ACTION" ] ; then
+    echo "No action specified!"
+    show_usage 
+    exit 3
+fi
 
 case $ACTION in
     up)
-        if [ ! -d "$2" ] ; then
+        if [ ! -d "$MOUNT_DIR" ] ; then
             mkdir -p $MOUNT_DIR
         fi
-        sshfs "$2:/" $MOUNT_DIR
+        sshfs "$REMOTE_HOST:/" $MOUNT_DIR
         ;;
     down)
         fusermount -u $MOUNT_DIR && rmdir $MOUNT_DIR
         ;;
     *)
-        echo "Impossible value for \$ACTION: $ACTION"
+        echo "Invalid value for \$ACTION: $ACTION"
         show_usage 
+        exit 4
         ;;
 esac
 
