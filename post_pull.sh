79da38279f811752f5ed29232cdf4ae22e11ed5a
Eric Petersen <eric@huskers.unl.edu>
2014-01-25 17:47:23 -0600

    Adding i3 config file

--- post_pull.sh
+++ post_pull.sh
@@ -10,15 +10,17 @@ source bash/post_pull_functions.sh
 create_symlinks () {
     echo "Creating symlinks to $(basename $REPODIR) files for $USERNAME on $HOSTNAME..."
 
+    safe_mkdir $HOME/.i3/
     safe_mkdir $HOME/bin/
     safe_mkdir $HOME/remotefs/
 
     # dot files
-    link_to_git_repo $HOME/.ackrc    $REPODIR/dotfiles/ackrc
-    link_to_git_repo $HOME/.bashrc   $REPODIR/dotfiles/bashrc
-    link_to_git_repo $HOME/.screenrc $REPODIR/dotfiles/screenrc
-    link_to_git_repo $HOME/.vimrc    $REPODIR/dotfiles/vimrc
-    link_to_git_repo $HOME/.zshrc    $REPODIR/dotfiles/zshrc
+    link_to_git_repo $HOME/.ackrc     $REPODIR/dotfiles/ackrc
+    link_to_git_repo $HOME/.bashrc    $REPODIR/dotfiles/bashrc
+    link_to_git_repo $HOME/.screenrc  $REPODIR/dotfiles/screenrc
+    link_to_git_repo $HOME/.vimrc     $REPODIR/dotfiles/vimrc
+    link_to_git_repo $HOME/.zshrc     $REPODIR/dotfiles/zshrc
+    link_to_git_repo $HOME/.i3/config $REPODIR/dotfiles/i3-config
 
     # programs/scripts
     link_to_git_repo $HOME/bin/latex2png      $REPODIR/latex/latex2png.sh
